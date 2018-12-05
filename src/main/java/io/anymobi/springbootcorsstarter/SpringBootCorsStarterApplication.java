3b40b36819d344626d4cd93823596a8357f5ec91
onjsdnjs <onjsdnjs@gmail.com>
2018-12-05 17:54:56 +0900

    Initial commit

---
+++ src/main/java/io/anymobi/springbootcorsstarter/SpringBootCorsStarterApplication.java
@@ -0,0 +1,12 @@
+package io.anymobi.springbootcorsstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCorsStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCorsStarterApplication.class, args);
+    }
+}
